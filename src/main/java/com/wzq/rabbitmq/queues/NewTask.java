736e7845c9ce6655352edc1774e9ea96107b91c6
zhenqing.wang <wzqwsrf@126.com>
2016-06-22 14:53:05 +0800

    add Work Queues

---
+++ src/main/java/com/wzq/rabbitmq/queues/NewTask.java
@@ -0,0 +1,43 @@
+package com.wzq.rabbitmq.queues;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Author: zhenqing.wang <wangzhenqing1008@163.com>
+ * Date: 2016-06-22 13:47:00
+ * Description: Work queues
+ */
+public class NewTask {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public void send() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+        for (int i = 0; i < 10; i++) {
+            String message = "Hello World " + i;
+            channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+        channel.close();
+        connection.close();
+    }
+
+    public static void main(String[] args) {
+        NewTask task = new NewTask();
+        try {
+            task.send();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/wzq/rabbitmq/queues/Worker2.java
@@ -0,0 +1,61 @@
+package com.wzq.rabbitmq.queues;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Author: zhenqing.wang <wangzhenqing1008@163.com>
+ * Date: 2016-06-22 14:12:30
+ * Description: Work queues
+ */
+public class Worker2 {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public void receive() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        channel.basicQos(1);
+        Consumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("Worker2 Received '" + message + "'");
+                try {
+                    doWork();
+                }finally {
+                    System.out.println("Worker2 [x] Done");
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                }
+            }
+        };
+        channel.basicConsume(TASK_QUEUE_NAME, false, consumer);
+    }
+
+    private void doWork() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void main(String[] args) {
+        Worker worker = new Worker();
+        try {
+            worker.receive();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+}
